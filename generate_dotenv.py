21a818d71ee789ac4dc7557251e3fda5335dbda2
pi3c <pi3c@yandex.ru>
2023-10-12 15:05:34 +0300

    цвета в консоли

--- generate_dotenv.py
+++ generate_dotenv.py
@@ -9,36 +9,23 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 
+def cls():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 with open(dotenv_path, "a") as f:
     if os.getenv("BRAND") is None:
-        br = input("Введите название блога: ")
+        cls()
+        print("\033[32m{}\033[0m ".format("Введите название проекта."))
+        print(
+            "Это название будет отображаться в строке",
+            "меню и футере на страницах сайта",
+            sep="\n",
+        )
+        br = input("-> ")
         f.writelines(f"BRAND={br}\n")
 
-    print("Генерирую SECRET_KEY...")
-    if os.getenv("SECRET_KEY") is None:
-        f.writelines(f"SECRET_KEY={uuid.uuid4().hex}\n")
-        print("_Ok_")
-    else:
-        print("SECRET_KEY уже установлен, пропускаю")
-
-    print('Генерирую "Соль"...')
-    if os.getenv("SECURITY_PASSWORD_SALT") is None:
-        f.writelines(f"SECURITY_PASSWORD_SALT={uuid.uuid4().hex}\n")
-        print("_Ok_")
-    else:
-        print("SECURITY_PASSWORD_SALT уже установлен, пропускаю")
-
-    if os.getenv("SQLALCHEMY_DATABASE_URI") is None:
-        print("Настроки подключения к базе данных Posgresql:")
-        login = input("Введите логин пользователя бд: ")
-        passwd = input("Пароль: ")
-        db = input("Название бд (по умолчанию pyprogerdb):") or "pyprogerdb"
-        ip = input("Адрес бд (по умолчанию localhost)") or "localhost"
-        port = input("Порт подключения: (по умолчанию 5432)") or "5432"
-        f.writelines(
-            f"SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://{login}:{passwd}@{ip}:{port}/{db}\n"
-        )
-
+        print("\033[32m{}\033[0m ".format("Настройка блока copyright в футере сайта"))
     if os.getenv("COPYRIGHT_YEAR") is None:
         start_date = dt.utcnow().strftime("%Y")
         f.writelines(f"COPYRIGHT_YEAR={start_date}\n")
@@ -61,3 +48,30 @@ with open(dotenv_path, "a") as f:
     if os.getenv("COPYRIGHT_CITY") is None:
         name = input("Введите свой город для для футера:")
         f.writelines(f"COPYRIGHT_CITY={name}\n")
+
+    print("\033[32m{}\033[0m ".format("Генерирую SECRET_KEY..."))
+    if os.getenv("SECRET_KEY") is None:
+        f.writelines(f"SECRET_KEY={uuid.uuid4().hex}\n")
+        print("_Ok_")
+    else:
+        print("SECRET_KEY уже установлен, пропускаю")
+
+    print("\033[32m{}\033[0m ".format('Генерирую "Соль"...'))
+    if os.getenv("SECURITY_PASSWORD_SALT") is None:
+        f.writelines(f"SECURITY_PASSWORD_SALT={uuid.uuid4().hex}\n")
+        print("_Ok_")
+    else:
+        print("SECURITY_PASSWORD_SALT уже установлен, пропускаю")
+
+    if os.getenv("SQLALCHEMY_DATABASE_URI") is None:
+        print(
+            "\033[32m{}\033[0m ".format("Настроки подключения к базе данных Posgresql:")
+        )
+        login = input("Введите логин пользователя бд: ")
+        passwd = input("Пароль: ")
+        db = input("Название бд (по умолчанию pyprogerdb):") or "pyprogerdb"
+        ip = input("Адрес бд (по умолчанию localhost)") or "localhost"
+        port = input("Порт подключения: (по умолчанию 5432)") or "5432"
+        f.writelines(
+            f"SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://{login}:{passwd}@{ip}:{port}/{db}\n"
+        )
